3a7366887f2abbcc18af7c8ed271545c4dc8433f
_shehab <54242265+shehabhesham123@users.noreply.github.com>
2024-04-28 02:04:58 +0300

    some update in bookmark feature

--- app/src/main/java/com/example/readerapp/feature/stories/ui/activity/ChoosePageNumberDialog.kt
+++ app/src/main/java/com/example/readerapp/feature/stories/ui/activity/ChoosePageNumberDialog.kt
@@ -8,30 +8,44 @@ import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.example.readerapp.databinding.BookmarkDialogBinding
 
-class ChoosePageNumberDialog(private val mnNumber: Int, private val mxNumber: Int) :
+class ChoosePageNumberDialog(
+    private val mnNumber: Int,
+    private val mxNumber: Int,
+    private val onlyExternal: Boolean
+) :
     DialogFragment() {
 
     private lateinit var clickListener: (value: Any) -> Unit
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val view = BookmarkDialogBinding.inflate(LayoutInflater.from(requireContext()), null, false)
-        view.numberPicker.maxValue = mxNumber
-        view.numberPicker.minValue = mnNumber
+
+        if (onlyExternal) {
+            view.numberPicker.isEnabled = false
+        } else {
+            view.numberPicker.maxValue = mxNumber
+            view.numberPicker.minValue = mnNumber
+        }
 
         view.ok.setOnClickListener {
             val url = view.externalLink.text.toString().trim()
-            val value =
+            if (onlyExternal) {
+                showToast("You can't enter the page number, only the url can enter it")
                 if (url.isNotEmpty() && (url.contains("http://") || url.contains("https://"))) {
-                    url
-                } else {
-                    Toast.makeText(
-                        requireContext(),
-                        "You entered the page number, not the url",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    view.numberPicker.value
+                    clickListener(url)
+                    dismiss()
                 }
-            clickListener(value)
-            dismiss()
+            } else {
+                val value =
+                    if (url.isNotEmpty() && (url.contains("http://") || url.contains("https://"))) {
+                        url
+                    } else {
+                        showToast("You entered the page number, not the url")
+                        view.numberPicker.value
+                    }
+                clickListener(value)
+                dismiss()
+            }
+
         }
         view.cancel.setOnClickListener {
             dismiss()
@@ -39,6 +53,14 @@ class ChoosePageNumberDialog(private val mnNumber: Int, private val mxNumber: In
         return AlertDialog.Builder(requireContext())
             .setView(view.root)
             .create()
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(
+            requireContext(),
+            message,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
     // fluent interface
