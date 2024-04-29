f3b77e53ff06bd94077a274818d233f198003176
_shehab <54242265+shehabhesham123@users.noreply.github.com>
2024-04-29 16:19:21 +0300

    some update

--- app/src/main/java/com/example/readerapp/feature/stories/ui/adapter/StoryDetialAdapter.kt
+++ app/src/main/java/com/example/readerapp/feature/stories/ui/adapter/StoryDetialAdapter.kt
@@ -3,27 +3,17 @@ package com.example.readerapp.feature.stories.ui.adapter
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
-import android.graphics.Typeface
 import android.text.Spannable
-import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.BackgroundColorSpan
-import android.text.style.ClickableSpan
-import android.text.style.ForegroundColorSpan
-import android.text.style.ImageSpan
-import android.text.style.StyleSpan
-import android.text.style.UnderlineSpan
 import android.util.TypedValue
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.readerapp.R
 import com.example.readerapp.core.navigation.Navigation
 import com.example.readerapp.databinding.PageBinding
 import com.example.readerapp.feature.auth.credentials.Authenticator
 import com.example.readerapp.feature.stories.data.model.Modifier
-import com.example.readerapp.feature.stories.data.model.ModifierName
 import com.example.readerapp.feature.stories.data.model.Page
 import com.example.readerapp.feature.stories.data.model.Query
 import com.example.readerapp.feature.stories.data.model.Story
@@ -153,67 +143,27 @@ class StoryDetailAdapter(
     }
 
     fun underline(page: Page) {
-        // the last modifiers in page.modifiers has the start and end of selection sentence
-        val lastModifier = page.modifiers[page.modifiers.size - 1]
-        // to able to cache the modifiers
-        lastModifier.modifierName = ModifierName.UNDERLINE
-        page.modifiers[page.modifiers.size - 1] = lastModifier
-        val underlineSpan = underlineSpan()
-        page.spannableString.setSpan(underlineSpan, lastModifier.start, lastModifier.end, 0)
+        page.underline()
         listener.onUnSelectionListener()
         notifyData()
-    }
-
-    private fun underlineSpan(): UnderlineSpan {
-        return UnderlineSpan()
     }
 
     fun highlighter(page: Page) {
-        // the last modifiers in page.modifiers has the start and end of selection sentence
-        val lastModifier = page.modifiers[page.modifiers.size - 1]
-        // to able to cache the modifiers
-        lastModifier.modifierName = ModifierName.HIGHLIGHTER
-        page.modifiers[page.modifiers.size - 1] = lastModifier
-        val highlightSpan = highlighterSpan(Color.YELLOW)
-        page.spannableString.setSpan(highlightSpan, lastModifier.start, lastModifier.end, 0)
+        page.highlighter()
         listener.onUnSelectionListener()
         notifyData()
-    }
-
-    private fun highlighterSpan(color: Int): BackgroundColorSpan {
-        return BackgroundColorSpan(color)
     }
 
     fun bold(page: Page) {
-        // the last modifiers in page.modifiers has the start and end of selection sentence
-        val lastModifier = page.modifiers[page.modifiers.size - 1]
-        // to able to cache the modifiers
-        lastModifier.modifierName = ModifierName.BOLD
-        page.modifiers[page.modifiers.size - 1] = lastModifier
-        val boldSpan = boldSpan()
-        page.spannableString.setSpan(boldSpan, lastModifier.start, lastModifier.end, 0)
+        page.bold()
         listener.onUnSelectionListener()
         notifyData()
-    }
-
-    private fun boldSpan(): StyleSpan {
-        return StyleSpan(Typeface.BOLD)
     }
 
     fun changeSentenceColor(page: Page, color: Int) {
-        // the last modifiers in page.modifiers has the start and end of selection sentence
-        val lastModifier = page.modifiers[page.modifiers.size - 1]
-        // to able to cache the modifiers
-        lastModifier.modifierName = ModifierName.CHANGE_SENTENCE_COLOR
-        page.modifiers[page.modifiers.size - 1] = lastModifier
-        val foregroundSpan = foregroundColorSpan(color)
-        page.spannableString.setSpan(foregroundSpan, lastModifier.start, lastModifier.end, 0)
+        page.changeSentenceColor(color)
         listener.onUnSelectionListener()
         notifyData()
-    }
-
-    private fun foregroundColorSpan(color: Int): ForegroundColorSpan {
-        return ForegroundColorSpan(color)
     }
 
     fun changeTextColor(color: Int) {
@@ -250,7 +200,7 @@ class StoryDetailAdapter(
             for (i in indices) {
                 val page = i / Page.PAGE_TEXT_SIZE + 1    // because first page for title
                 val start = i % Page.PAGE_TEXT_SIZE
-                val highlightSpan = highlighterSpan(Color.TRANSPARENT)
+                val highlightSpan = BackgroundColorSpan(Color.TRANSPARENT)
                 pages[page].spannableString.setSpan(highlightSpan, start, start + wordSize, 0)
             }
         }
@@ -258,39 +208,8 @@ class StoryDetailAdapter(
     }
 
     fun bookMark(page: Page, goTo: Any) {
-        // the last modifiers in page.modifiers has the start and end of selection sentence
-        val lastModifier = page.modifiers[page.modifiers.size - 1]
-        // to able to cache the modifiers
-        lastModifier.modifierName = ModifierName.BOOK_MARK
-        page.modifiers[page.modifiers.size - 1] = lastModifier
-        val clickSpan = clickSpan(goTo)
-        val iconSpan = iconSpan()
-        page.spannableString.setSpan(clickSpan, lastModifier.start, lastModifier.end, 0)
-        page.spannableString.setSpan(iconSpan, lastModifier.end, lastModifier.end + 1, 0)
+        page.bookMark(recyclerView, navigation, goTo)
         notifyData()
-    }
-
-    private fun clickSpan(goTo: Any): ClickableSpan {
-        return object : ClickableSpan() {
-            override fun onClick(p0: View) {
-                if (goTo is Int)
-                    recyclerView.smoothScrollToPosition(goTo)
-                else if (goTo is String) {
-                    navigation.showExternalUrl(goTo, recyclerView.context)
-                }
-            }
-
-            override fun updateDrawState(ds: TextPaint) {
-                super.updateDrawState(ds)
-
-                val color = context.resources.getColor(R.color.link, null)
-                ds.color = color
-            }
-        }
-    }
-
-    private fun iconSpan(): ImageSpan {
-        return ImageSpan(context, R.drawable.external_link)
     }
 
     @SuppressLint("NotifyDataSetChanged")
